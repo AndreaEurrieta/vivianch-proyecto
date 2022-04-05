fcd953fea1d66d98ae3c4c73f16b18d71f95ccbe
Andrea Eurrieta <deyeurrieta@gmail.com>
2022-04-05 10:59:43 -0300

    itemcount commit

---
+++ src/components/ItemListContainer/ItemCount/ItemCount.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "React";
+
+export const ItemCount=({ stock, initial, onAdd })=>{
+    const [count, setCount] = useState(initial);
+
+    const handleAdd = () => {
+        if (count < stock) {
+            setCount(count + 1);
+        }
+    }
+    const handleRemove = () => {
+        if (count > initial) {
+            setCount(count - 1);
+        }
+    }
+
+    return (
+        <div className="btn-group" role="group" aria-label="Basic outlined example">
+            <button onClick={handleAdd} type="button" className="btn btn-outline-primary">+</button>
+            <imput type="number" className="form-control">{count}</imput>
+            <button onClick={handleRemove} type="button" className="btn btn-outline-primary">-</button>
+            <button onClick={onAdd(count)} type="button" className="btn btn-primary">Agregar al carrito</button>
+        </div>
+    )
+}
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,12 @@
+import { ItemCount } from './ItemCount/ItemCount';
+
 export const ItemListContainer= ({greeting}) =>{
     return(
         <div>
             <h1>{greeting}</h1>
+            <div>
+                <ItemCount initial={1} stock={8} onAdd={onAdd}/>
+            </div>
         </div>
     )
 }
