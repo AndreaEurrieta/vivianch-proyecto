a0c92604bbe7a97a6e436bed49a103a554770c81
deyluimar eurrieta <deyeurrieta@gmail.com>
2022-05-05 23:02:35 -0300

    firebase install

---
+++ src/services/firebase/index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv5sntFq1TypCM6Qy_gnR_mbtgYyU-GEc",
+  authDomain: "vivianch-proyecto.firebaseapp.com",
+  projectId: "vivianch-proyecto",
+  storageBucket: "vivianch-proyecto.appspot.com",
+  messagingSenderId: "278673227554",
+  appId: "1:278673227554:web:e773d92935fb238f326f7b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const firestoreDb = getFirestore(app)
